d611d082fa8792e8e29917ecc156312f7a713239
ernest-lamptey <1ernestlamptey@gmail.com>
2023-11-13 12:07:53 +0100

    set up navigation in js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Set the links on the navigation up so that when you click one the display status is turned on and the rest are turned off.
+
+const home = document.querySelector("#intro-link");
+const works = document.querySelector("#works-link");
+const contact = document.querySelector("#contact-link")
+
+const turnOnDislay = (link, arr) => {
+    arr.forEach(elem => {
+        const elemSection = document.getElementById(elem.href.split("#")[1])
+        if(link === elem) {
+            elemSection.style.display = "block";
+        } else {
+            elemSection.style.display = "none"
+        }
+    });
+}
+
+const links = [home, works, contact]
+
+links.forEach(link => {
+    link.addEventListener("click", (e) => {
+        turnOnDislay(e.target, links);
+    })
+})
